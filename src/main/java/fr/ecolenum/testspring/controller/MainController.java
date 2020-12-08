46887a285d325d7b84176348bee940ef790197b0
Ludivine-O <ludivine.ottini@le-campus-numerique.com>
2020-12-08 11:27:31 +0100

    Affichage de la liste des voitures, et route vers chacune des voiture en ajoutant l'id de chaque voiture à la fin de l'adresse

--- src/main/java/fr/ecolenum/testspring/controller/MainController.java
+++ src/main/java/fr/ecolenum/testspring/controller/MainController.java
@@ -8,6 +8,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -23,11 +25,17 @@ public class MainController {
     @Value("${welcome.message}")
     private String message;
 
-    @RequestMapping(value={"/"}, method = RequestMethod.GET)
+    @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("message", message);
         model.addAttribute("cars", cars);
         return "index";
+    }
+
+    @GetMapping("/{id}")
+    public String car(Model model, @PathVariable("id") int id){
+        model.addAttribute("car", cars.get(id));
+        return "car";
     }
     
 
